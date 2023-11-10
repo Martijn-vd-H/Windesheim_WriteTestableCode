b58575d4d8eab6a42c9b108b0c72e0722b138ed7
Martijn van den Hoven <mvandenhoven@vitas.nl>
2023-11-10 15:59:00 +0100

    Apply OCP to pricing

--- WriteTestableCode/3. OCP/PriceCalculator.cs
+++ WriteTestableCode/3. OCP/PriceCalculator.cs
@@ -6,22 +6,10 @@ public class PriceCalculator
 {
     public int Calculate(OrderParameters orderParameters)
     {
-        var price = 0;
-        switch (orderParameters.Type)
+        if (Constants.HardwareTypes.TryGetValue(orderParameters.Type, out var type))
         {
-            case HardwareType.Laptop:
-                price = 1200 * orderParameters.Number;
-                break;
-            case HardwareType.Monitor:
-                price = 250 * orderParameters.Number;
-                break;
-            case HardwareType.Desk:
-                price = 550 * orderParameters.Number;
-                break;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(orderParameters.Type), orderParameters.Type, null);
+            return type * orderParameters.Number;
         }
-
-        return price;
+        throw new ArgumentOutOfRangeException(nameof(orderParameters.Type), orderParameters.Type, "Type not implemented in pricing dictionary yet");
     }
 }
---
+++ WriteTestableCode/SingleResponsibility/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace WriteTestableCode.SingleResponsibility;
+
+public static class Constants
+{
+    public static readonly Dictionary<HardwareType, int> HardwareTypes = new()
+    {
+        {HardwareType.Laptop, 1200},
+        {HardwareType.Monitor, 250},
+        {HardwareType.Desk, 500},
+    };
+}
